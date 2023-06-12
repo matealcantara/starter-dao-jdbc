7aa7ca150d235e7f2cba87b965f710e2b6e9e09d
Mateus Alcântara <matalcantara.dev@gmail.com>
2023-06-12 12:56:07 -0400

    findByIdImplementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	public static SellerDao createSellerDao() {
 		// TODO Auto-generated method stub
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
